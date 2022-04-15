e3faf30891f34fdff132bbf866475bd5a723271d
Antonina <petrovi-orenburg@mail.ru>
2022-04-16 01:15:25 +0500

    Задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = -9;
+int b = -3;
+if (a>b) 
+    {
+    Console.Write("max = ");
+    Console.WriteLine($"{a}");
+    }
+    else
+    {
+    System.Console.Write("max = ");
+    System.Console.WriteLine($"{b}");   
+    }
