b21af59244f9024683b0971a0f778234458cf18e
Antonina <petrovi-orenburg@mail.ru>
2022-04-16 02:56:12 +0500

    Задание 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿int N = 8; 
+int i = 2;
+
+while (i <= N)
+{
+    if (N %2 == 0) 
+    {
+    Console.WriteLine(i);
+    }
+    else 
+    {
+        Console.WriteLine("2");
+        N = N - 1;
+    }
+ i+=2;
+}
