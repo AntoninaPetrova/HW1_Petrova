a0223df5b928d9927dd7bc3b2c0b07941ab61cdd
Antonina <petrovi-orenburg@mail.ru>
2022-04-16 13:04:04 +0500

    Задание 2

---
+++ Seminar1/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random ().Next (10, 100);
+int number2 = new Random ().Next (2, 5);
+if (number%number2 == 0)
+{
+    System.Console.WriteLine($"Число {number} кратно числу {number2}");
+}
+else {
+    System.Console.WriteLine($"Число {number} не кратно числу {number2}.");
+    System.Console.WriteLine($"остаток {number%number2}");
+}
