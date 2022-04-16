33708a9f72f1f1e3a3d9cea005c1394fed94688f
Antonina <petrovi-orenburg@mail.ru>
2022-04-16 12:52:21 +0500

    Первая задача семинара

---
+++ Seminar/Program.cs
@@ -0,0 +1,6 @@
+﻿//int [] array = new int [10, 99];
+//new Random ().Next
+int x = new Random ().Next (10, 100);
+Console.Write(x + ",");
+if (x % 10 > x / 10) System.Console.WriteLine(x % 10);
+else System.Console.WriteLine( x / 10); 
