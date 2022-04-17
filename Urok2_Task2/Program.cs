0ba4726ec7280c1d2a918c6ec47dcd088dcd1012
Antonina <petrovi-orenburg@mail.ru>
2022-04-17 22:42:41 +0500

    Добавила комментарии к заданиям

--- Urok2_Task2/Program.cs
+++ Urok2_Task2/Program.cs
@@ -1,4 +1,8 @@
-﻿int number = new Random().Next(100, 999);
+﻿//Напишите программу, которая выводит
+//случайное трёхзначное число и удаляет вторую цифру
+//этого числа.
+
+int number = new Random().Next(100, 999);
 System.Console.WriteLine(number);
 
 int b = number / 100 % 10;
