2c05e668f87a1da781a51d3bb213d2d6e84ac7db
Antonina <petrovi-orenburg@mail.ru>
2022-04-25 15:33:16 +0500

    Задание 27

---
+++ Urok4_Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+int Sum(int number)
+{
+int sum = 0;
+    while (number > 0)
+    {
+    sum = sum + number %10; 
+    number = number / 10;  
+    }    
+
+  return sum;
+}
+System.Console.WriteLine(Sum(9012)); 
