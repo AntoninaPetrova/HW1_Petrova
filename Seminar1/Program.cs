96482277b41097a4f0a7b66edd6c8b6ca1ca6d8a
Antonina <petrovi-orenburg@mail.ru>
2022-04-25 14:13:42 +0500

    Задание 25

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,10 +1,35 @@
-﻿int number = new Random ().Next (10, 100);
-int number2 = new Random ().Next (2, 5);
-if (number%number2 == 0)
-{
-    System.Console.WriteLine($"Число {number} кратно числу {number2}");
-}
-else {
-    System.Console.WriteLine($"Число {number} не кратно числу {number2}.");
-    System.Console.WriteLine($"остаток {number%number2}");
-}
+﻿//int number = new Random ().Next (10, 100);
+//int number2 = new Random ().Next (2, 5);
+//if (number%number2 == 0)
+//{
+//    System.Console.WriteLine($"Число {number} кратно числу {number2}");
+//}
+//else {
+//    System.Console.WriteLine($"Число {number} не кратно числу {number2}.");
+//    System.Console.WriteLine($"остаток {number%number2}");
+//
+
+//void Method1()
+//{
+//Console.WriteLine("Автор Петрова");
+//}
+//Method1();
+
+//void Method2(string msg)
+//{
+//    System.Console.WriteLine(msg);
+//}
+//Method2("Nhfnfnf");
+
+//void Method21(string msg, int count)
+//{
+//    int = 0;
+//    while (i < count)
+//    {
+//     System.Console.WriteLine(msg); 
+//       i++;
+//    }
+//}   
+//Method21(msg: "Текст", count: 4);
+
+int SumToA(int A)
---
+++ Urok4_Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите (функцию) цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int Power (int A, int B)
+{
+int power = 1;
+
+for (int i = 0; i < B; i++)
+{
+    power = A * power;
+}
+return power;
+}
+System.Console.WriteLine(Power(3, 5)); 
